591142fa8355915b6a5c7d25f0561ee84e37557d
Trevor Thompson <trevor.t3000@gmail.com>
2026-08-29 09:09:36 -0400

    Update error handling and verbose output for new site command.

--- src/commands.rs
+++ src/commands.rs
@@ -3,7 +3,7 @@ use std::os::unix::fs as unix_fs;
 use std::process::Command as ShellCommand;
 use std::fs::OpenOptions;
 use std::io::Write;
-use std::path::Path;
+use std::path::{Path};
 use regex::Regex;
 use tokio::runtime::{Builder};
 use crate::{db, prompt};
@@ -11,6 +11,8 @@ use crate::db::get_default_config;
 use crate::template::get_apache_config_template;
 
 pub fn new_site(domain: &String, verbose: u8) {
+    let vprint = get_verbose_conditional_print(verbose);
+
     // let username = env::var("USER").unwrap();
 
     sudo::with_env(&["USER", "RUST_BACKTRACE"]).unwrap();
@@ -61,19 +63,24 @@ pub fn new_site(domain: &String, verbose: u8) {
     }
 
     // create database
-    // let db_result = db::create_database(&db_name, get_default_config(&db_pass));
+    match Builder::new_current_thread().enable_all().build() {
+        Ok(rt) => {
+            let db_result = rt.block_on(
+                db::create_database(&db_name, get_default_config(&db_pass))
+            );
 
-    let rt = Builder::new_current_thread()
-        .enable_all()
-        .build()
-        .unwrap();
-
-    let db_result = rt.block_on(
-        db::create_database(&db_name, get_default_config(&db_pass))
-    );
-
-    if db_result.is_err() {
-        println!("Failed to create database: {}", db_result.unwrap_err());
+            match db_result {
+                Ok(_) => {
+                    vprint(1, "Database created")
+                },
+                Err(err) => {
+                    eprintln!("Database error: {}", err);
+                }
+            }
+        },
+        Err(e) => {
+            eprintln!("Thread builder error: {}", e);
+        }
     }
 
     // TODO: If is_wp install and configure wordpress
@@ -90,17 +97,13 @@ pub fn new_site(domain: &String, verbose: u8) {
         link_doc_root.push_str(&after.to_string());
     }
 
-    if verbose > 1 {
-        println!("Symlink doc root: {}", link_doc_root);
-    }
+    vprint(1, format!("Symlink doc root: {}", link_doc_root).as_str());
 
     let conf_path = format!("/etc/apache2/sites-available/{}.conf", domain);
     let enabled_conf_path = format!("/etc/apache2/sites-enabled/{}.conf", domain);
 
-    if verbose >= 1 {
-        println!("Conf path: {}", conf_path);
-        println!("Enabled conf path: {}", enabled_conf_path);
-    }
+    vprint(1, format!("Conf path: {}", conf_path).as_str());
+    vprint(1, format!("Enable conf path: {}", enabled_conf_path).as_str());
 
     let apache_conf = get_apache_config_template(
         domain,
@@ -112,64 +115,73 @@ pub fn new_site(domain: &String, verbose: u8) {
     if Path::new(&conf_path).exists() {
         println!("Site configuration already exists: {}", conf_path);
     } else {
-        if verbose >= 2 {
-            println!("Creating site config: {}", conf_path)
-        }
+        vprint(2, format!("Creating site config: {}", conf_path).as_str());
 
         fs::write(&conf_path, &apache_conf).unwrap();
     }
 
-    if verbose >= 1 {
-        println!("Creating symbolic link: {} -> {}", conf_path, enabled_conf_path);
-    }
+    vprint(1, format!("Creating symbolic link: {} -> {}", conf_path, enabled_conf_path).as_str());
 
     // create site-enabled symlink
     if Path::new(&enabled_conf_path).exists() {
         println!("Site configuration already exists: {}", enabled_conf_path);
     } else {
-        unix_fs::symlink(&conf_path, &enabled_conf_path).unwrap();
+        match unix_fs::symlink(&conf_path, &enabled_conf_path) {
+            Ok(_) => {
+                vprint(1, "Symlink created")
+            },
+            Err(err) => {
+                eprintln!("Apache config symlink creation failed: {}", err);
+            }
+        }
     }
 
     let www_path = String::from(format!("/var/www/html/{}", domain));
     if Path::new(&www_path).exists() {
         println!("Skipping symbolic link. Path exists: {}", www_path);
     } else {
-        if verbose >= 2 {
-            println!("Creating symbolic link: {}", www_path);
-        }
+        vprint(2, format!("Creating symbolic link: {}", www_path).as_str());
 
         // symlink site path and /var/www/html/site.domain
-        _ = unix_fs::symlink(site_path, &www_path);
+        match unix_fs::symlink(site_path, &www_path) {
+            Ok(_) => {
+                vprint(1, "Web root symlink created")
+            },
+            Err(err) => {
+                eprintln!("Web root symlink creation failed: {}", err);
+            }
+        }
     }
 
-    if verbose >= 1 {
-        println!("Mapping DNS...")
-    }
+    vprint(1, "Mapping DNS...");
 
     let dns_entry = format!("127.0.0.1 {}", domain);
 
     let dns_content = fs::read_to_string("/etc/hosts");
-    if dns_content.unwrap().contains(&dns_entry) {
-        if verbose >= 1 {
-            println!("Hosts file already contains entry: {}", dns_entry);
-        }
-    } else {
-        // append dnsEntry to hosts file
-        let hosts_file_create = OpenOptions::new()
-            .append(true)
-            .open("/etc/hosts");
+    match dns_content {
+        Ok(content) => {
+           if content.contains(&dns_entry) {
+               vprint(1, format!("Hosts file already contains entry: {}", dns_entry).as_str());
+           } else {
+               // append dnsEntry to hosts file
+               let hosts_file_create = OpenOptions::new()
+                   .append(true)
+                   .open("/etc/hosts");
 
-        match hosts_file_create {
-            Ok(mut file) => {
-                _ = file.write_all(dns_entry.as_bytes());
+               match hosts_file_create {
+                   Ok(mut file) => {
+                       _ = file.write_all(dns_entry.as_bytes());
 
-                if verbose >= 1 {
-                    println!("Hosts file updated: {}", dns_entry);
-                }
-            },
-            Err(_) => {
-                println!("Failed to write to hosts file: {}", dns_entry);
-            }
+                       vprint(1, format!("Hosts file updated: {}", dns_entry).as_str());
+                   },
+                   Err(_) => {
+                       println!("Failed to write to hosts file: {}", dns_entry);
+                   }
+               }
+           }
+        },
+        Err(err) => {
+            eprintln!("Failed reading /etc/hosts: {}", err);
         }
     }
 
@@ -183,24 +195,39 @@ pub fn new_site(domain: &String, verbose: u8) {
         Ok(output) => {
             if !output.status.success() {
                 println!("Failed to restart Apache service: {}", output.status);
-            } else if verbose >= 1 {
-                println!("Apache service restarted");
+            } else {
+                vprint(1, "Apache service restarted");
             }
         },
         Err(e) => {
-            println!("Service restart failed with error: {}", e)
+            eprintln!("Service restart failed with error: {}", e)
         }
     }
-
 
     println!("Setup complete: {}", domain);
 }
 
+fn get_verbose_conditional_print(level: u8) -> impl Fn(u8, &str) {
+    // move to give ownership of the captured values to the closure
+    move | threshold, msg | {
+        if level >= threshold {
+            println!("{}", msg);
+        }
+    }
+}
+
 // replace with underscores
 fn slugify(s: &str) -> String {
-    let re = Regex::new(r"[^a-zA-Z0-9_-]").unwrap();
+    let re = Regex::new(r"[^a-zA-Z0-9_-]");
 
-    re.replace_all(s, "_").into_owned()
+    match re {
+        Ok(regex) => {
+            regex.replace_all(s, "_").into_owned()
+        },
+        Err(_) => {
+            s.to_string()
+        },
+    }
 }
 
 fn prompt_for_php_version() -> String {
@@ -226,33 +253,44 @@ fn prompt_for_wordpress_install() -> bool {
 }
 
 // Prompts for a path in a loop, until either:
-// The input is empty, so it returns the PWD
+// The input is empty, so it returns the CWD
 // A valid existing path is entered
 // Tha absolute path with trailing slash trimmed is returned
 fn prompt_for_directory_input(msg: &str) -> String {
     loop {
         let mut input = prompt::get_string(msg);
 
-        let current_dir = env::current_dir().unwrap();
+        let current_dir = env::current_dir();
 
-        // if input is empty, return PWD
-        if input.is_empty() {
-            if current_dir.is_dir() {
-                return normalize_path(
-                    current_dir.to_str().unwrap()
-                ).to_string();
+        match current_dir {
+            // panic here is fine.
+            // if we're not getting path's right, we shouldn't continue
+            Ok(current_dir) => {
+                // if input is empty, return CWD
+                if input.is_empty() {
+                    if current_dir.is_dir() {
+                        return normalize_path(
+                            current_dir.to_str().unwrap()
+                        ).to_string();
+                    }
+                }
+
+                if !input.starts_with("/") {
+                    let dir_str = current_dir.to_str()
+                        .unwrap()
+                        .to_string();
+                    input = format!("{}/{}", dir_str, input);
+                }
+
+                if Path::new(&input).exists() {
+                    return normalize_path(&input).to_string();
+                } else {
+                    println!("{} does not exist", input);
+                }
+            },
+            Err(err) => {
+                panic!("Directory read error: {}", err);
             }
-        }
-
-        if !input.starts_with("/") {
-            let dir_str = current_dir.to_str().unwrap().to_string();
-            input = format!("{}/{}", dir_str, input);
-        }
-
-        if Path::new(&input).exists() {
-            return normalize_path(&input).to_string();
-        } else {
-            println!("{} does not exist", input);
         }
     }
 }
